3234a2f45d7f26ffc239c37d2d0b83804ac78226
Junyeong Heo <hhjjyy1122@gmail.com>
2025-11-26 18:42:00 +0900

    chore: API user request 함수 수정

--- src/api/user.ts
+++ src/api/user.ts
@@ -2,3 +2,19 @@
 export function getUserProfileById(userId: string) {
   // GET /user/:userId
 }
+
+export function leaveCrew(crewId: string) {
+  // PUT /user/leave/
+  // body: { crewId }
+}
+
+export function expelMember(crewId: string, userId: string) {
+  // PUT /user/expel/
+  // body: { crewId, userId }
+}
+
+export function updateMember(crewId: string, userId: string, role: string) {
+  // PATCH /user/update/
+  // body: { crewId, userId, role }
+  // user가 crew의 leader일 때만 가능
+}
