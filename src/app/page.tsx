a9bd4ce8aa4662b8567a549589596f7c9ac3b351
Junyeong Heo <hhjjyy1122@gmail.com>
2025-11-28 18:22:29 +0900

    chore: 기본 폴더 구조와 하위 파일 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,9 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
-import { Dialog } from '@/components/ui/dialog';
-
 export default function Home() {
   return (
     <div className="flex h-screen justify-center items-center">
-      <Dialog>
-        <Dialog.DialogTrigger>Open</Dialog.DialogTrigger>
-        <Dialog.DialogContent>
-          <Dialog.DialogHeader>
-            <Dialog.DialogTitle>Are you absolutely sure?</Dialog.DialogTitle>
-            <Dialog.DialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </Dialog.DialogDescription>
-          </Dialog.DialogHeader>
-        </Dialog.DialogContent>
-      </Dialog>
+      Hello, world!
     </div>
   );
 }
