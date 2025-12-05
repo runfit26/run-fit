42235d6d6c1b7334b6816343d8e12ea0ae20cdc0
seoleunjin <anah9026@gmail.com>
2025-12-05 15:50:06 +0900

    refactor: 공통 디자인 변수 추가 및 타이포그래피 유틸리티 정리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@
 export default function Home() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <p className="text-display-lg-semibold bg-brand-700 p-5">Hello, world!</p>
+      <p className="text-title1-semibold bg-brand-500 tablet:bg-brand-600 laptop:bg-brand-700 desktop:bg-gray-200 p-5">
+        Hello, world!
+      </p>
     </div>
   );
 }
