550f2c5027b580b537ceece5257daae568134c4d
Junyeong Heo <hhjjyy1122@gmail.com>
2025-12-09 09:58:25 +0900

    fix: storybook에서 vite-plugin-svgr 사용해서 svg를 storybook vite 번들링에 포함하도록 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,18 @@
 import type { StorybookConfig } from '@storybook/nextjs-vite';
+import svgr from 'vite-plugin-svgr';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: ['@storybook/addon-docs', '@storybook/addon-a11y'],
   framework: '@storybook/nextjs-vite',
   staticDirs: ['../public'],
+  viteFinal: (config) => {
+    config?.plugins?.push(
+      svgr({
+        include: '**/*.svg?react',
+      })
+    );
+    return config;
+  },
 };
 export default config;
