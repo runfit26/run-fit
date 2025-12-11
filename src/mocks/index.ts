57e51e3a6f7672d96c93633af19c17a10e73a44d
Junyeong Heo <hhjjyy1122@gmail.com>
2025-12-11 16:07:30 +0900

    feat: 환경변수 추가 - development에서 MSW 실행방지

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,5 +1,12 @@
 export async function initMocks() {
   if (process.env.NODE_ENV !== 'development') return;
+
+  // Allow disabling MSW in development via environment variable
+  if (process.env.USE_MSW === 'false') {
+    console.log('[MSW] Mocks disabled via USE_MSW=false');
+    return;
+  }
+
   try {
     const { server } = await import('./server');
     server.listen();
