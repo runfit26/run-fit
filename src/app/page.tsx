9fe2968f5b8f16ed454c2d3cb7663330fcbf4fa1
Seokjun.Kang <kangsj0327@gmail.com>
2025-12-11 13:39:26 +0900

    chore: 홈페이지 mock api 테스트용 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,9 @@
-'use client';
-
-import { useEffect, useState } from 'react';
-import { Crew } from '@/types/crew';
-
 export default function HomePage() {
-  const [crews, setCrews] = useState<Crew[] | null>(null);
-
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch('/api/crews');
-      const data = await response.json();
-      setCrews(data.data);
-      console.log(data);
-    }
-    fetchData();
-  }, []);
-
   return (
     <main className="h-main flex items-center justify-center">
       <p className="text-title1-semibold bg-brand-500 tablet:bg-brand-600 laptop:bg-brand-700 desktop:bg-gray-200 p-5">
         Hello, world!
       </p>
-
-      {crews &&
-        crews.map((crew) => (
-          <div key={crew.id} className="text-white">
-            {crew.name}
-          </div>
-        ))}
     </main>
   );
 }
