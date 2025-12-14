c1d9e6b7c0437f6be06814e37bddacd6dccb1252
Yun-Jinwoo <yjw3480@naver.com>
2025-12-14 17:11:06 +0900

    feat: user Mutation hook 구현

---
+++ src/api/mutations/userMutations.ts
@@ -0,0 +1,15 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { updateMyProfile } from '@/api/fetch/user';
+import { userQueries } from '@/api/queries/userQueries';
+
+// 내 정보 수정
+export function useUpdateMyProfile() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: updateMyProfile,
+    onSuccess: (updatedUserData) => {
+      queryClient.setQueryData(userQueries.me().queryKey, updatedUserData); // 업데이트된 데이터 캐시를 직접 업데이트
+    },
+  });
+}
