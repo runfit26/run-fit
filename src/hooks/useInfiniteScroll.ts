74d7c79bcc6d341375fa52d174d6ccabd9e59f34
Shion <seoy1108@gmail.com>
2025-12-24 17:28:12 +0900

    refactor: isFetching 상태를 고려하여 무한 스크롤 콜백 조건 수정

--- src/hooks/useInfiniteScroll.ts
+++ src/hooks/useInfiniteScroll.ts
@@ -1,13 +1,8 @@
+import { useIsFetching } from '@tanstack/react-query';
 import { useEffect, useRef } from 'react';
 
 /**
  * 특정 요소가 뷰포트(화면)에 진입했을 때 자동으로 callback을 호출하는 무한 스크롤용 커스텀 훅
- *
- * 사용 예시:
- * ```tsx
- *  const loadMoreRef = useInfiniteScroll(() => fetchNextPage(), hasNextPage);
- *  <div ref={loadMoreRef} />
- * ```
  *
  * @param callback - 요소가 화면에 나타났을 때 실행할 함수 (예: fetchNextPage)
  * @param enabled - 무한 스크롤 동작 여부를 제어하는 값 (예: hasNextPage)
@@ -15,20 +10,28 @@ import { useEffect, useRef } from 'react';
  */
 export function useInfiniteScroll(callback: () => void, enabled = true) {
   const ref = useRef<HTMLDivElement | null>(null);
+  const isFetching = useIsFetching();
 
   useEffect(() => {
     if (!enabled || !ref.current) return;
 
-    const observer = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting) {
-        callback();
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const entry = entries[0];
+
+        if (entry.isIntersecting && isFetching === 0) {
+          callback();
+        }
+      },
+      {
+        threshold: 0.25, // 요소가 25% 보일 때 콜백 실행
       }
-    });
+    );
 
     observer.observe(ref.current);
 
     return () => observer.disconnect();
-  }, [callback, enabled]);
+  }, [callback, enabled, isFetching]);
 
   return ref;
 }
