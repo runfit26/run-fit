89ad12202e69d03265ac6050d2b3e1c81c580f4c
Shion <seoy1108@gmail.com>
2025-12-31 13:51:59 +0900

    fix: Jest setup 파일 ES Modules 에러 해결 및 top-level await 제거

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,13 +1,24 @@
 import '@testing-library/jest-dom';
-import './src/mocks/nextNavigation';
 import { jest } from '@jest/globals';
 
 globalThis.jest = jest;
 
-if (process.env.NEXT_PUBLIC_USE_MSW === 'true') {
-  const { server } = await import('@/mocks/jest');
+// next/navigation mock
+export const pushMock = jest.fn();
+jest.mock('next/navigation', () => ({
+  useRouter: () => ({
+    push: pushMock,
+    replace: jest.fn(),
+    prefetch: jest.fn(),
+  }),
+}));
 
-  beforeAll(() => server.listen());
-  afterEach(() => server.resetHandlers());
-  afterAll(() => server.close());
+if (process.env.NEXT_PUBLIC_USE_MSW === 'true') {
+  beforeAll(async () => {
+    const { server } = await import('@/mocks/jest');
+    server.listen();
+
+    afterEach(() => server.resetHandlers());
+    afterAll(() => server.close());
+  });
 }
