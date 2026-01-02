2f88386a84f712fd3b6f4b7fd47930a153508ee5
Seokjun.Kang <kangsj0327@gmail.com>
2026-01-03 00:38:18 +0900

    refactor: 크루 찾기 페이지 Layout과 Header 컴포넌트 디자인에 맞게 개선

--- src/app/crews/page.tsx
+++ src/app/crews/page.tsx
@@ -37,7 +37,7 @@ export default function Page() {
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="h-main mx-auto flex max-w-[1120px] flex-col items-center justify-start px-6">
+    <main className="h-main mx-auto flex max-w-[1120px] flex-col items-center justify-start p-6">
       <Header />
       {children}
       <CreateCrewButton />
@@ -47,14 +47,14 @@ function Layout({ children }: { children: React.ReactNode }) {
 
 function Header() {
   return (
-    <div className="flex w-full items-center justify-between">
+    <div className="tablet:flex hidden w-full items-center justify-between">
       <div className="my-[45px]">
-        <h2 className="text-title2-semibold mb-4">
+        <h1 className="text-title2-semibold mb-1 text-gray-50">
           나와 FIT한 러닝 메이트를 찾다
-        </h2>
-        <span className="text-body3-regular text-gray-200">
-          러닝 페이스와 선호하는 스타일에 딱 맞는 크루를 찾아보세요!
-        </span>
+        </h1>
+        <p className="text-body3-regular text-gray-200">
+          나와 잘 맞는 러닝 크루를 찾아보세요!
+        </p>
       </div>
     </div>
   );
