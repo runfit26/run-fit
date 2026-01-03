2530d46c0c808fb54a11c6364427ff7558e10c08
Shion <seoy1108@gmail.com>
2026-01-03 18:50:55 +0900

    refactor: 불필요한 Suspense 제거

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,17 +1,7 @@
-import { Suspense } from '@suspensive/react';
 import SignupForm from '@/components/auth/SignupForm';
 import AuthLayout from '@/components/layout/AuthLayout';
-import Spinner from '@/components/ui/Spinner';
 
 export default function SignupPage() {
-  return (
-    <Suspense fallback={<Spinner.Page />}>
-      <SignupClient />
-    </Suspense>
-  );
-}
-
-function SignupClient() {
   return (
     <AuthLayout
       title="회원가입"
